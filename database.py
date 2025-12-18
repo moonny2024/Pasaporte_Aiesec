0d4465521bcb7bf521fc5c228f64926a05be30ed
joestevil <mejiadigita@gmail.com>
2025-12-18 16:41:38 -0500

    main joe

--- database.py
+++ database.py
@@ -8,5 +8,6 @@ def init_db(app):
     app.config['MYSQL_PASSWORD'] = ''
     app.config['MYSQL_DB'] = 'db_passport'
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+    app.config['MYSQL_PORT'] = 3336
 
     mysql.init_app(app)
